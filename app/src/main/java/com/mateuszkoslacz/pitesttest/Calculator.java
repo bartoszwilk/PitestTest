cf18206ed29a3ec6e401f18bd857a44c6a4cb90a
mkoslacz <mateusz.koslacz@grupawp.pl>
2017-03-10 20:51:34 +0100

    sample calculator class with Android gradle pitest config

---
+++ app/src/main/java/com/mateuszkoslacz/pitesttest/Calculator.java
@@ -0,0 +1,24 @@
+package com.mateuszkoslacz.pitesttest;
+
+/**
+ * Created by mateuszkoslacz on 10.03.2017.
+ */
+
+public class Calculator {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int substract(int a, int b){
+        return a - b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a, int b){
+        return a / b;
+    }
+}
